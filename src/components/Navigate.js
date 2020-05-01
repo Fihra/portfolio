3a3241464034509ce044b8267624a2cd1d44fc95
Fabian <fabrofabian@gmail.com>
2020-05-01 04:10:57 -0700

    switched nav to material ui tabs

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -2,108 +2,39 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+import Fade from '@material-ui/core/Fade';
+
 import About from './About';
 
 class Navigate extends Component {
     constructor(props){
         super(props);
         this.state = {
-            isAboutOn: false,
-            isAudioOn: false,
-            isWebOn: false,
-            isContactOn: false
+            selection: "About"
         }
     }
 
-    showPage = (contentSelection) => {
-        switch(contentSelection){
-            case "About":
-                this.controlAbout();
-                return;
-            case "Audio":
-                this.controlAudio();
-                return;
-            case "Web":
-                this.controlWeb();
-                return;
-            case "Contact":
-                this.controlContact();
-                return;
-            default:
-                break;
-        }
-    }
-
-    controlAbout = () => {
-        if((this.state.isAudioOn || this.state.isWebOn || this.state.isContactOn) || (this.state.isAboutOn === false )){
-            this.setState({
-                isAboutOn: true,
-                isAudioOn: false,
-                isWebOn: false,
-                isContactOn: false
-            })
-        } else{
-            this.setState({
-                isAboutOn: false
-            })
-        }  
-    }
-
-    controlAudio = () => {
-        if((this.state.isAboutOn || this.state.isWebOn || this.state.isContactOn) || (this.state.isAudioOn === false )){
-            this.setState({
-                isAboutOn: false,
-                isAudioOn: true,
-                isWebOn: false,
-                isContactOn: false
-            })
-        } else{
-            this.setState({
-                isAudioOn: false
-            })
-        }  
-    }
-
-    controlWeb = () => {
-        if((this.state.isAboutOn || this.state.isAudioOn || this.state.isContactOn) || (this.state.isWebOn === false )){
-            this.setState({
-                isAboutOn: false,
-                isAudioOn: false,
-                isWebOn: true,
-                isContactOn: false
-            })
-        } else{
-            this.setState({
-                isWebOn: true
-            })
-        }  
-    }
-
-    controlContact = () => {
-        if((this.state.isAboutOn || this.state.isAudioOn || this.state.isWebOn) || (this.state.isContactOn === false )){
-            this.setState({
-                isAboutOn: false,
-                isAudioOn: false,
-                isWebOn: false,
-                isContactOn: true
-            })
-        } else{
-            this.setState({
-                isContactOn: false
-            })
-        }  
+    handleChange = (event, newVal) => {
+        console.log(newVal);
+        this.setState({
+            selection: newVal
+        })
     }
 
     render(){
         return(
             <div>
-                <ul className="navigator">
-                    <li><Button onClick={() => this.showPage("About")}>About</Button></li>
-                    <li><Button onClick={() => this.showPage("Audio")}>Technical Audio Portfolio</Button></li>
-                    <li><Button onClick={() => this.showPage("Web")}>Web Portfolio</Button></li>
-                    <li><Button onClick={() => this.showPage("Contact")}>Contact</Button></li>
-                </ul>
-            </div>
+                <Tabs value={this.state.selection} onChange={this.handleChange} aria-label='wrapped label tabs example'>
+                    <Tab value="About" label="About"/>
+                    <Tab value="Audio" label="Audio Portfolio"/>
+                    <Tab value="Web" label="Web Portfolio"/>
+                    <Tab value="Contact" label="Contact"/>
+                </Tabs>
+                {this.state.selection === "About" ? <Fade in={true} timeout={{enter: 4000, exit: 4000}}><About /></Fade> : null}
+            </div> 
         )
     }
 }
