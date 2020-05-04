7e29ece039d4f1e63302aedc461e3a8fdaf7d20b
Fihra <fabrofabian@gmail.com>
2020-05-04 02:46:19 -0700

    Updates

---
+++ precache-manifest.9ca9fa8c0ebb0f13bc22c94cffc0a1b5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d572ee5bf8e8bc395e028d4ce69d11",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e7c7f0228a18cb82fbb",
+    "url": "/portfolio/static/css/main.cceb2f7a.chunk.css"
+  },
+  {
+    "revision": "5484a5ba034b0558a1a7",
+    "url": "/portfolio/static/js/2.12924617.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.12924617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7c7f0228a18cb82fbb",
+    "url": "/portfolio/static/js/main.a9d4baf6.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "919db6b01d1204f9de6a4b27bc81b144",
+    "url": "/portfolio/static/media/DIY_or_Die.919db6b0.jpg"
+  },
+  {
+    "revision": "e215fb796ce924445acd56b142804ca9",
+    "url": "/portfolio/static/media/DeathByDebt.e215fb79.png"
+  },
+  {
+    "revision": "593bc77244d5c1515bb3ca14c8d27091",
+    "url": "/portfolio/static/media/FMOD_Audio_Integration_Demo.593bc772.jpg"
+  },
+  {
+    "revision": "137fc29f227211dfd8674daa40e21eac",
+    "url": "/portfolio/static/media/Instrument_Collection.137fc29f.jpg"
+  },
+  {
+    "revision": "0f384bcdeec1895492e1ac57eed07f86",
+    "url": "/portfolio/static/media/MusicInspire.0f384bcd.jpg"
+  },
+  {
+    "revision": "597e92b2e1898fb5da9cb71689c781be",
+    "url": "/portfolio/static/media/MusicianDirectory.597e92b2.jpg"
+  },
+  {
+    "revision": "02c0ff3590f098a5383d180f84ce3644",
+    "url": "/portfolio/static/media/OshaCoaster.02c0ff35.jpg"
+  },
+  {
+    "revision": "490bffd7a9e9602806953f2398fcc290",
+    "url": "/portfolio/static/media/Sidetracked.490bffd7.jpg"
+  },
+  {
+    "revision": "bd2803d2e9773aa4012acaab8493a6e4",
+    "url": "/portfolio/static/media/headshot.bd2803d2.jpg"
+  },
+  {
+    "revision": "bdbf1c292950769298ce5051d752bea0",
+    "url": "/portfolio/static/media/pathfinder_laststand.bdbf1c29.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/build/precache-manifest.5122dd0be2459023e92a926aafeb1fc8.js"
+  "/portfolio/precache-manifest.9ca9fa8c0ebb0f13bc22c94cffc0a1b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
