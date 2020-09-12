e14ff0c092b36fa0262c90f9bc4b6a8685c197d7
Fihra <fabrofabian@gmail.com>
2020-09-11 22:29:22 -0700

    opened build

---
+++ build/precache-manifest.7da39c805982fb061e50a9b08fe2770f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0632e00bd58a9963676f5e651e53820",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5746ff624187cf3ca996",
+    "url": "/portfolio/static/css/main.cceb2f7a.chunk.css"
+  },
+  {
+    "revision": "611d5028ce1d8a17b7e8",
+    "url": "/portfolio/static/js/2.adcfa766.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.adcfa766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5746ff624187cf3ca996",
+    "url": "/portfolio/static/js/main.c3f9de1e.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "a8f3f243f2d2d91a9cdeada23a75388e",
+    "url": "/portfolio/static/media/Asteroid_Defense.a8f3f243.png"
+  },
+  {
+    "revision": "919db6b01d1204f9de6a4b27bc81b144",
+    "url": "/portfolio/static/media/DIY_or_Die.919db6b0.jpg"
+  },
+  {
+    "revision": "e215fb796ce924445acd56b142804ca9",
+    "url": "/portfolio/static/media/DeathByDebt.e215fb79.png"
+  },
+  {
+    "revision": "593bc77244d5c1515bb3ca14c8d27091",
+    "url": "/portfolio/static/media/FMOD_Audio_Integration_Demo.593bc772.jpg"
+  },
+  {
+    "revision": "137fc29f227211dfd8674daa40e21eac",
+    "url": "/portfolio/static/media/Instrument_Collection.137fc29f.jpg"
+  },
+  {
+    "revision": "0f384bcdeec1895492e1ac57eed07f86",
+    "url": "/portfolio/static/media/MusicInspire.0f384bcd.jpg"
+  },
+  {
+    "revision": "597e92b2e1898fb5da9cb71689c781be",
+    "url": "/portfolio/static/media/MusicianDirectory.597e92b2.jpg"
+  },
+  {
+    "revision": "02c0ff3590f098a5383d180f84ce3644",
+    "url": "/portfolio/static/media/OshaCoaster.02c0ff35.jpg"
+  },
+  {
+    "revision": "490bffd7a9e9602806953f2398fcc290",
+    "url": "/portfolio/static/media/Sidetracked.490bffd7.jpg"
+  },
+  {
+    "revision": "30cf03e63830bbdf7f41799397806662",
+    "url": "/portfolio/static/media/culture-awareness-101.30cf03e6.png"
+  },
+  {
+    "revision": "bd2803d2e9773aa4012acaab8493a6e4",
+    "url": "/portfolio/static/media/headshot.bd2803d2.jpg"
+  },
+  {
+    "revision": "bdbf1c292950769298ce5051d752bea0",
+    "url": "/portfolio/static/media/pathfinder_laststand.bdbf1c29.png"
+  }
+]);
